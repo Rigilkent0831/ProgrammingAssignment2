6f1548fe71af319b336b613901b78fbbd930fb71
Rigilkent0831 <agus_kurniawan0831@yahoo.com>
2021-08-22 18:26:01 +0700

    push an already updated cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -2,20 +2,27 @@
 ## inverse, and then cache it for later use 
 
 ## The "makeCacheMatrix" function will create a "matrix" object which contains
-## 2 pairs of "mutato" and "accessor" for Matrix and it's inverse respectively
+## 2 pairs of "mutator" and "accessor" for Matrix and it's inverse respectively
 
 makeCacheMatrix <- function(x = matrix()) {
         x.inv <- NULL
-# Mutator and Accessor of matrix object      
+# Mutator and Accessor of matrix object
+# setmatrix is used to assign a new matrix "x" for calculation and 
+# also reset the cached inverse of matrix "x.inv"
         setmatrix <- function(y){
                 x <<- y
                 x.inv <<- NULL       
         }
-        
+#retrieve the latest matrix used for the calculation        
         getmatrix <- function() x
+        
 # Mutator and Accessor of inverse of the matrix object
+# setinverse is used to create a function to be use by "cacheSolve" function 
+# to assign the result of inverse matrix calculation to "x.inv"
         setinverse <- function(inverse) x.inv <<- inverse
         getinverse <- function() x.inv
+# generate a list of functions consited of 2 pairs of Mutators and Accessors
+# for matrix and its matrix inverse.
         list(setmatrix = setmatrix, getmatrix = getmatrix,
              setinverse = setinverse,
              getinverse = getinverse)
